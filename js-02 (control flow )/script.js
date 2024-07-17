c1a1ab91d8e375a5af85a7db9ae6d6d1a9cd7dbc
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-17 12:42:13 +0530

    add new file

---
+++ js-02 (control flow )/script.js
@@ -0,0 +1,157 @@
+// let score = prompt("enter the marks: ");
+
+// if (score>=90) {
+//     console.log("Grade: A");
+// }
+// else if(score>=75){
+//     console.log("Grade: B");
+// }
+// else if (score>=65){
+//     console.log("Grade: C");
+// }
+// else {
+//     console.log("you fail the exam");
+// }
+
+// let number = prompt("enter your number: ");
+
+// if (number>0) {
+//     console.log(`${number} is positive number`);
+// }
+// else {
+//     console.log(`${number} is negative number`);
+// }
+
+// let age= prompt("enter your age: ");
+
+// if(age>=18) {
+//     console.log("you are adult");
+// }
+// else {
+//     console.log("you are minor");
+// }
+
+// let number= prompt("enter your number: ");
+
+// if(number%2==0){
+//     console.log(`${number} is even number`);
+// }
+// else {
+//     console.log(`${number} is odd number`);
+// }
+
+// let rating=prompt("enter your rate: ");
+
+// if(rating<=2){
+//     console.log("bad rating");
+// }else if (rating>=4) {
+//     console.log("good rating");
+// }
+// else {
+//     console.log("average rating");
+// }
+
+// let marks = prompt("enter your marks: ");
+
+// if(marks>=40){
+//     if(marks>=80){
+//         console.log("Distinction");
+//     }
+//     else{
+//         console.log("passed");
+//     }
+// }
+// else {
+//     console.log("you fail the exam");
+// }
+
+// for(let i=1; i<6; i++){
+//     console.log(i);
+// }
+
+// for(let i=5; i<=50; i=i+5) {
+//     console.log(i);
+// }
+
+// let n=prompt("enter your number: ");
+// let sum=0;
+// for(let i=1; i<=n; i++){
+//     sum+=i;
+// }
+// console.log(`sum ${sum}`);
+
+// let fruits = ["apple","mango","banana"];
+
+// for(let i=0; i<fruits.length;i++) {
+//     console.log(fruits[i]);
+// }
+
+// let i=1;
+// while (i<=3) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i=3;
+// do {
+//     console.log(i);
+//     i--;
+// } while(i>0)
+
+// let i=5;
+// while(i<=50) {
+//     console.log(i);
+//     i=i+5;
+// }
+
+// let num=parseInt(prompt("enter number: "));
+
+// let count = 1;
+
+// while(count<=10){
+//     let product = count * num;
+//     console.log(`${num} * ${count} = ${product}`);
+//     count++;
+// }
+
+// let num = 5;
+// let count = 1;
+// do {
+//   let product = num * count;
+//   console.log(product);
+//   count++;
+// } while (count <= 10);
+
+// for (let i = 1; i <= 5; i++) {
+//   if (i == 3) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (let i=1; i<=10; i++) {
+//     if(i%2==0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i=1; i<=10; i++) {
+//     if(i>4 && i<9) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let num=1;
+
+// while(num<=10) {
+//     if(num % 2 === 0){
+//         ++num;
+//         continue;
+
+//     }
+//     console.log(num);
+//     ++num;
+
+// }
