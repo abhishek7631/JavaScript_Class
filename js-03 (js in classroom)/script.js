d3970a1711f907f20b3491d7d4f3e457ce902b99
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-26 00:02:30 +0530

    folder add

---
+++ js-03 (js in classroom)/script.js
@@ -0,0 +1,125 @@
+//creating object literals.            ******************
+
+// let student = {
+//   name: "abhishek",
+//   age: 23,
+//   marks: 87,
+// };
+
+// console.log(student);
+
+// const item = {
+//   price: 100,
+//   discount: 30,
+//   colors: ["red", "blue", "green"],
+// };
+
+// console.log(item);
+
+//creating a post.                    ******************
+
+// const post = {
+//   username: "@abhishek",
+//   content: "this is my #first post",
+//   like: 10,
+//   repost: 5,
+//   tags: ["@ayush", "@yoges"],
+// };
+
+// console.log(post);
+
+//get values.                       ******************
+
+// const post = {
+//   username: "@abhishek",
+//   content: "this is my #first post",
+//   like: 10,
+//   repost: 5,
+//   tags: ["@ayush", "@yoges"],
+// };
+
+// console.log(post);
+
+// console.log(post.content);
+// console.log(post.like);
+// console.log(post["tags"]);
+
+///conversion in get values.                       ******************
+//value automatic change into strings.
+
+// let obj = {
+//   1: "a",
+//   2: "b",
+//   true: "c",
+//   null: "d",
+//   undefined: "e",
+// };
+
+// console.log(obj);
+// console.log(obj[1]);
+// console.log(obj[2]);
+// console.log(obj[null]);
+
+// add/update value.                  ******************
+
+// let student = {
+//   name: "abhishek",
+//   age: 23,
+//   marks: 87,
+//   city: "delhi",
+// };
+
+// console.log(student);
+// console.log((student.city = "bhopal"));
+// console.log((student.gender = "male"));
+// console.log((student.marks = "A"));
+// console.log(delete student.city);
+// console.log((student.city = ["delhi", "pune", "bihar"]));
+
+// console.log(student);
+
+//nested objects(object of objects).                  ******************
+
+// const classInfo = {
+//   abhishek: {
+//     grade: "A",
+//     city: "pune",
+//   },
+//   ayush: {
+//     grade: "B",
+//     city: "aurangabaad",
+//   },
+//   yogesh: {
+//     grade: "C",
+//     city: "delhi",
+//   },
+// };
+
+// console.log(classInfo);
+// console.log(classInfo.abhishek);
+// console.log(classInfo.ayush);
+// console.log(classInfo.ayush.city);
+
+//array of objects.                 ******************
+
+// const classInfo = [
+//   {
+//     name: "abhsihek",
+//     grade: "O",
+//     city: "bihar",
+//   },
+//   {
+//     name: "ayush",
+//     grade: "A+",
+//     city: "pune",
+//   },
+//   {
+//     name: "yogesh",
+//     grade: "A",
+//     city: "Aurangabaad",
+//   },
+// ];
+
+// console.log(classInfo);
+
+// math object.                  ******************
