c0bafc00b3802a89989bce5097682c629aa4558e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-26 19:37:32 +0530

    code modified

--- Strings_based_Practice_Question/script.js
+++ Strings_based_Practice_Question/script.js
@@ -33,13 +33,70 @@
 
 //Q.4  JavaScript Program to Reverse a String      ***********************
 
-function reverseString(str) {
-  let newstr = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    newstr += str[i];
+// function reverseString(str) {
+//   let newstr = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newstr += str[i];
+//   }
+//   return newstr;
+// }
+
+// let string = prompt("enter your string");
+// console.log(`reverse string of ${string} is:`, reverseString(string));
+
+//Q.5  JavaScript Program to Check the Number of Occurrences of a Character in the String       **************************
+
+// function countString(str, letter) {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str.charAt(i) == letter) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
+
+// let str = prompt("enter your string");
+// let letter = prompt("enter a letter to check");
+
+// console.log(
+//   `in this string ${str} , the letter of ${letter} is:`,
+//   countString(str, letter)
+// );
+
+//Q.6  JavaScript Program to Convert the First Letter of a String into UpperCase.               ***************************
+
+// without using function.
+// let str = "abhishek";
+
+// let capital = str.charAt(0).toUpperCase() + str.slice(1);
+// console.log(capital);
+
+//using function.
+// function capital(str) {
+//   let capital = str.charAt(0).toUpperCase() + str.slice(1);
+//   return capital;
+// }
+
+// let string = prompt("enter your string");
+// console.log(capital(string));
+
+//Q.7   JavaScript Program to Count the Number of Vowels in a String.     ***********************
+
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count += 1;
+    }
   }
-  return newstr;
+
+  return count;
 }
 
-let string = prompt("enter your string");
-console.log(`reverse string of ${string} is:`, reverseString(string));
+let inputString = prompt("Enter your string");
+console.log(
+  `The number of vowels in the string is: ${countVowels(inputString)}`
+);
