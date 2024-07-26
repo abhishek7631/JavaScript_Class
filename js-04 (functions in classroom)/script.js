309c87cd2b9a047c24bba287b09fcc01f8e9b594
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-26 22:31:54 +0530

    code modified

---
+++ js-04 (functions in classroom)/script.js
@@ -0,0 +1,56 @@
+// Basics of functions         ****************
+
+// function hello() {
+//   console.log("hello");
+// }
+
+// hello();
+// hello();
+// hello();
+// hello();
+
+// function printNmae() {
+//   console.log("abhishek");
+//   console.log("choudhary");
+// }
+
+// printNmae();
+
+// function print1to5() {
+//   for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+//   }
+// }
+
+// print1to5();
+
+// function isAdult() {
+//   let age = 18;
+//   if (age >= 18) {
+//     console.log("adult");
+//   } else {
+//     console.log("not adult");
+//   }
+// }
+
+// isAdult();
+
+//creats a function that prints a poem.          ***************
+
+// function printPoem() {
+//   console.log("twinkle twinkle little star");
+//   console.log("how i wonder that you are");
+// }
+
+// printPoem();
+
+//create a function to roll dice and always display the value of the dice (1 to 6).         ******************
+
+function rolldice() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+}
+
+rolldice();
+
+// functions with arguments.                ******************
