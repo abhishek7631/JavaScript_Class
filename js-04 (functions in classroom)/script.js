7e67b8136543732e7d5e6b27cebd2593427e1181
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-27 20:25:10 +0530

    code modified

--- js-04 (functions in classroom)/script.js
+++ js-04 (functions in classroom)/script.js
@@ -153,15 +153,51 @@
 // console.log(hello());
 
 // high order functions.     ****************
+//(takes one or multiple functions as a arguments).
 
-function multipleGreet(fun, n) {
-  for (let i = 1; i <= n; i++) {
-    fun();
-  }
-}
+// function multipleGreet(fun, n) {
+//   for (let i = 1; i <= n; i++) {
+//     fun();
+//   }
+// }
 
-let greet = function () {
-  console.log("hello");
+// let greet = function () {
+//   console.log("hello");
+// };
+
+// console.log(multipleGreet(greet, 2));
+
+// high order functions.     ****************
+//(return a functions).
+
+// function oddEvenTest(request) {
+//   if (request == "odd") {
+//     return function (n) {
+//       console.log(n % 2 !== 0);
+//     };
+//   } else if (request == "even") {
+//     return function (n) {
+//       console.log(n % 2 == 0);
+//     };
+//   } else {
+//     console.log("wrong request");
+//   }
+// }
+
+// let request = "even";
+
+//Methods           ****************
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
 };
 
-console.log(multipleGreet(greet, 2));
+console.log(calculator.mul(2, 3));
