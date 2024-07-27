fbafd37c31595dee73ec06f34e9e290493bca1ef
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-27 23:11:10 +0530

    code modified

--- js-04 (functions in classroom)/script.js
+++ js-04 (functions in classroom)/script.js
@@ -201,3 +201,22 @@ const calculator = {
 };
 
 console.log(calculator.mul(2, 3));
+
+//Q. Write a JavaScript function that returns array elements larger than a number.   ****************
+
+// let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
+// let num = 5;
+
+// function getElements(arr, num) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > num) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(getElements(arr, num));
+
+//Q.2 Write a JavaScript function to extract unique characters from a string.     ******************
