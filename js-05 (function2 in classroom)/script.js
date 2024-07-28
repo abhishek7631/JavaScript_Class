846662baea3b8d6caa7c18b4d083b8df17413ccd
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-28 13:37:23 +0530

    add folder

---
+++ js-05 (function2 in classroom)/script.js
@@ -0,0 +1,54 @@
+// 'this' in javascript.                ********************
+
+// const student = {
+//   name: "abhishek",
+//   age: 23,
+//   eng: 78,
+//   phy: 98,
+//   math: 87,
+//   getAvg() {
+//     let avg = (this.eng + this.phy + this.math) / 3;
+//     console.log(avg);
+//   },
+// };
+
+// console.log(student.getAvg());
+
+// try and catch.               **********************
+
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// try {
+//   console.log(a);
+// } catch {
+//   console.log("variable is not define");
+// }
+
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+
+//Arrow functions.                     ***************************
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// console.log(sum(2, 3));
+
+// **************
+
+// const cube = (n) => {
+//   console.log(n * n * n);
+// };
+
+// console.log(cube(3));
+
+// ***************
+
+// const power = (a, b) => {
+//   console.log(a ** b);
+// };
+
+// console.log(power(2, 4));
