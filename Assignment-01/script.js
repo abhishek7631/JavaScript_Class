873db620e870c54344830d4377daccb37b9b8a68
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-31 16:15:15 +0530

    add code in the folder

---
+++ Assignment-01/script.js
@@ -0,0 +1,38 @@
+//Question 1:
+//Convert the array arr = [1, 2, 3, 4, 5] into a string with each element separated by a hyphen (-).
+
+// let arr = [1, 2, 3, 4, 5];
+// let str = arr.toString();
+// console.log(str);
+// let joined = arr.join("-");
+// console.log(joined);
+
+//Question 2:
+//Join the array names = ["Alice", "Bob", "Charlie"] into a single string with each name separated by a comma (,).
+
+// let arr = ["Alice", "Bob", "Charlie"];
+
+// let joined = arr.join(",");
+
+// console.log(joined);
+
+//Question 3:
+//Remove the last element from the array arr = [1, 2, 3, 4, 5].
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// let Remove = arr.pop();
+// console.log(Remove);
+// console.log(arr);
+
+//Question 4:
+//Add the number 6 to the end of the array arr = [1, 2, 3, 4, 5].
+
+// console.log("before adding number:");
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// console.log("add number in end:");
+// let added = arr.push(6);
+// console.log(added);
+// console.log("after adding number:");
+// console.log(arr);
