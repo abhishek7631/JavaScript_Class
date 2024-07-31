e97f18684bdbae06b103837da3e5115f11eed97e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-07-31 16:41:15 +0530

    add code

--- Assignment-01/script.js
+++ Assignment-01/script.js
@@ -36,3 +36,51 @@
 // console.log(added);
 // console.log("after adding number:");
 // console.log(arr);
+
+//Question 5:
+//Remove the first element from the array arr = [1, 2, 3, 4, 5].
+
+// console.log("before removing the number:");
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// let remove = arr.shift();
+// console.log("remove number:");
+// console.log(remove);
+// console.log("after removing the number");
+// console.log(arr);
+
+//Question 6:
+//Add the number 0 to the beginning of the array arr = [1, 2, 3, 4, 5].
+
+// console.log("before adding the number:");
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// let addNumber = arr.unshift(0);
+// console.log(addNumber);
+// console.log("after adding the number");
+// console.log(arr);
+
+//Question 7:
+//Delete the element at index 2 from the array arr = [1, 2, 3, 4, 5].
+
+// console.log("before deleting the element:");
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+// let Delete = arr.splice(2, 1);
+// console.log("delete index 2 element:");
+// console.log(Delete);
+// console.log("after deleting the element:");
+// console.log(arr);
+
+//Question 8:
+//Merge the arrays arr1 = [1, 2, 3] and arr2 = [4, 5, 6].
+
+// console.log("this is arr1:");
+// let arr1 = [1, 2, 3];
+// console.log(arr1);
+// console.log("this is arr2:");
+// let arr2 = [4, 5, 6];
+// console.log(arr2);
+// console.log("after merging both arrays:");
+// let mergeArray = arr1.concat(arr2);
+// console.log(mergeArray);
