f15422355e996a9130a733651e91877c72fb66ae
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-06 23:08:12 +0530

    code modified

--- js-07 (DOM-1 in classroom)/script.js
+++ js-07 (DOM-1 in classroom)/script.js
@@ -5,5 +5,14 @@
 //   console.log(`value is changed of ${i}`);
 // }
 
-let para = document.getElementsByTagName("p")[1];
-console.log(para);
+// let para = document.getElementsByTagName("p")[1];
+// console.log(para);
+
+//query selectors   *************
+
+console.dir(document.querySelector("p"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelector("div a"));
+console.dir(document.querySelectorAll("div a"));
