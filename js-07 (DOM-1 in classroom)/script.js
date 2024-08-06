89f24d41263ee118a860678c6fd35eaab956c263
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-06 22:56:14 +0530

    add folder

---
+++ js-07 (DOM-1 in classroom)/script.js
@@ -0,0 +1,9 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for (let i = 0; i < smallImages.length; i++) {
+//   smallImages[i] = "assets/spiderman_img.png";
+//   console.log(`value is changed of ${i}`);
+// }
+
+let para = document.getElementsByTagName("p")[1];
+console.log(para);
