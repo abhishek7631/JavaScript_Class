65cb5edd43c77ba8cdc08e3e427cf439f4a306f5
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-12 23:39:43 +0530

    code modified

--- Arrays_and_Objects_based_Practice_Question/index.js
+++ Arrays_and_Objects_based_Practice_Question/index.js
@@ -349,6 +349,12 @@
 
 // console.log(result);
 
+// let str = "i love coding ";
+
+// let result = str.replaceAll("coding", "sleeping");
+
+// console.log(result);
+
 let str = "i love coding ";
 
 let result = str.replaceAll("coding", "sleeping");
