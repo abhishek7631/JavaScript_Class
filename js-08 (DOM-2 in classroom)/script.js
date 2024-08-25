0f2716b899eed314265973891099b36f52412a9e
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-25 15:48:36 +0530

    code modified

---
+++ js-08 (DOM-2 in classroom)/script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("button");
+
+// btn.onclick = function () {
+//   console.log("button was clicked");
+// };
+
+function saysHello() {
+  alert("hello");
+}
+
+btn.onclick = saysHello;
