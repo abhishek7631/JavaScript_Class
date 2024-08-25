0bea7dfb4e69f0fba51f44f3cb786a2787661470
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-25 16:47:27 +0530

    code modified

--- js-08 (DOM-2 in classroom)/script.js
+++ js-08 (DOM-2 in classroom)/script.js
@@ -1,11 +1,50 @@
+let h1 = document.querySelector("h1");
+
+let h3 = document.querySelector("h3");
+
+let p = document.querySelector("p");
+
 let btn = document.querySelector("button");
+
+function changeColor() {
+  console.log(this.innerText);
+  this.style.backgroundColor = "blue";
+}
+
+h1.addEventListener("click", changeColor);
+
+h3.addEventListener("click", changeColor);
+
+p.addEventListener("click", changeColor);
+
+btn.addEventListener("click", changeColor);
+
+// h1.addEventListener("click", function () {
+//   console.log(this.innerText);
+//   this.style.backgroundColor = "blue";
+// });
+
+// h3.addEventListener("click", function () {
+//   console.log(this.innerText);
+//   this.style.backgroundColor = "blue";
+// });
+
+// p.addEventListener("click", function () {
+//   console.log(this.innerText);
+//   this.style.backgroundColor = "blue";
+// });
+
+// btn.addEventListener("click", function () {
+//   console.log(this.innerText);
+//   this.style.backgroundColor = "blue";
+// });
 
 // btn.onclick = function () {
 //   console.log("button was clicked");
 // };
 
-function saysHello() {
-  alert("hello");
-}
+// function saysHello() {
+//   alert("hello");
+// }
 
-btn.onclick = saysHello;
+// btn.onclick = saysHello;
