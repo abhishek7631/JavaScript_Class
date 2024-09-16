bb2f3da39de8c437daf5cd9279ae847c23ff00ce
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-16 22:51:53 +0530

    code modified

--- Strings_based_Practice_Question/script.js
+++ Strings_based_Practice_Question/script.js
@@ -223,3 +223,17 @@
 // }
 
 // console.log(genrateRandomString(5));
+
+//Q.11   JavaScript Program to Generate Random String.               **********************
+
+// function genrateRandomString(length) {
+//   let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   let res = "";
+//   for (let i = 0; i < length; i++) {
+//     let randomIndex = Math.floor(Math.random() * char.length);
+//     res += char[randomIndex];
+//   }
+//   return res;
+// }
+
+// console.log(genrateRandomString(5));
