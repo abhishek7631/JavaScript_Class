5f3dfc8d5cf48d2032acdd0fc28696a8fee321a9
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-11 23:02:15 +0530

    code modified

--- control_flow_based_Practice_Question/script.js
+++ control_flow_based_Practice_Question/script.js
@@ -444,13 +444,3 @@
 //     console.log(fibonacci(i));
 //   }
 // }
-
-// const nTerms = prompt("Enter the number of terms: ");
-
-// if (nTerms <= 0) {
-//   console.log("Enter a positive integer.");
-// } else {
-//   for (let i = 0; i < nTerms; i++) {
-//     console.log(fibonacci(i));
-//   }
-// }
